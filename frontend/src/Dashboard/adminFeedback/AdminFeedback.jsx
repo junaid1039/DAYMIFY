2b8716c50c6a93979f5c971f929cad22d74c03de
junaid1039 <ssmbag103955@gmail.com>
2024-11-24 22:01:27 +0500

    .

--- frontend/src/Dashboard/adminFeedback/AdminFeedback.jsx
+++ frontend/src/Dashboard/adminFeedback/AdminFeedback.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './AdminFeedback.css';
+import './adminFeedback.css';
 import { FaStar, FaStarHalfAlt, FaRegStar } from 'react-icons/fa'; // Icons for stars
 
 const AdminFeedback = () => {
