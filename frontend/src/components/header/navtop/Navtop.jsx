a4112d0c770dadaed93ecf64c783b6206e653e92
junaid1039 <ssmbag103955@gmail.com>
2025-07-14 16:05:53 +0500

    done

--- frontend/src/components/header/navtop/Navtop.jsx
+++ frontend/src/components/header/navtop/Navtop.jsx
@@ -4,7 +4,7 @@ import './navtop.css';
 const Navtop = () => {
   return (
    <div className="nav">
-    <h5>Deployed on Vercel — Admin Dashboard at /admin (testuer@gmail.com | Testuser1).
+    <h5>Deployed on Vercel — Admin Dashboard access at /admin (testuser@gmail.com | Testuser1).
     </h5>
    </div>
   )
